477522dc167960b1817f17a8dcc5f9c57b2afe79
romazan26 <130604263+romazan26@users.noreply.github.com>
2024-11-05 10:40:14 +0300

    Initial Commit

---
+++ CryptoTrack/CryptoTrackApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryptoTrackApp.swift
+//  CryptoTrack
+//
+//  Created by Роман on 05.11.2024.
+//
+
+import SwiftUI
+
+@main
+struct CryptoTrackApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
